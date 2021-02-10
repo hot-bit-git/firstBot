96b2cc83481c5f3a5714e350620b8d5481775cc5
Roman <roman@junczys.krakow.pl>
2021-02-10 12:01:37 +0100

    I synchronised view of out bot with view of stellar site. I add function - values of pair xlm / xrp xrp / xlm

--- offers.py
+++ offers.py
@@ -1,5 +1,6 @@
 import json
 import time
+from quotes import values_of_pair
 from stellar_sdk import Server
 
 
@@ -7,12 +8,16 @@ server = Server(horizon_url="https://horizon.stellar.org")
 account_id = "GCSQRCJR77FHDITYON46VO5J47ZEOD5HXNM6FL3LJNTXAQ4D53G5DW6C"
 account_name = 'Kozak'
 
-account_id = "GA5OGZYOBTXBWTE5A6LXUCCWMUP7LYIS252DGEKBQ4IMFMKPTVMEK7LA"
-account_name = 'JakieśKonto'
+# account_id = "GA5OGZYOBTXBWTE5A6LXUCCWMUP7LYIS252DGEKBQ4IMFMKPTVMEK7LA"
+# account_name = 'JakieśKonto'
 
 key_last_modified_time = 'last_modified_time'
 key_embedded = '_embedded'
 key_records = 'records'
+key_amount = 'amount'
+key_buying = 'buying'
+key_selling = 'selling'
+key_price = 'price'
 
 
 def name_of_cryptocurrency(in_offers_data):
@@ -22,32 +27,71 @@ def name_of_cryptocurrency(in_offers_data):
         return in_offers_data['asset_code']
 
 
+def is_buying_XML(in_offers_data):
+    if in_offers_data['buying']['asset_type'] == 'native':
+        return True
+    else:
+        return False
+
+
+
 def selling_buying_offer(account_id):
     offers = server.offers().for_seller(account_id).call()
     offers_data = offers[key_embedded][key_records]
 
     # For control
-    print(json.dumps(offers, indent=4))
+    # print(json.dumps(offers, indent=4))
 
     for i in range(len(offers_data)):
 
-        sentence =  ' '
+        sentence =  ''
 
         sentence += offers_data[i]['last_modified_time']
         sentence += ' '
         sentence += account_name
-        sentence += " sells "
-        sentence += offers_data[i]['amount']
-        sentence += ' '
-        sentence += name_of_cryptocurrency(offers_data[i]['selling'])
-        sentence += ' '
 
-        sentence += 'for the price of '
-        sentence +=offers_data[i]['price']
-        sentence += ' '
+        # Decision if buying XLM or selling XLM. Buying means that will be more XLM. Selling means it will be less XLM
+        if(is_buying_XML(offers_data[i])):
 
-        sentence +='of '
-        sentence += name_of_cryptocurrency(offers_data[i]['buying'])
+            sentence += " BUYING "
+
+            amount_of_XLS = float(offers_data[i][key_amount])*float(offers_data[i][key_price])
+            sentence += "{:.8f}".format(amount_of_XLS)
+
+            sentence += ' '
+
+            sentence += name_of_cryptocurrency(offers_data[i][key_buying])
+            sentence += ' '
+
+            price_of_XRP_XLS = 1/float(offers_data[i][key_price])
+
+            sentence += ' for '
+            sentence += name_of_cryptocurrency(offers_data[i][key_selling])
+
+            sentence += ' at the price of '+"{:.8f}".format(price_of_XRP_XLS)
+
+            sentence += name_of_cryptocurrency(offers_data[i][key_buying])
+            sentence += '/'
+            sentence += name_of_cryptocurrency(offers_data[i][key_selling])
+
+        else:
+
+            sentence += " SELLING "
+            sentence += offers_data[i]['amount']
+            sentence += ' '
+            sentence += name_of_cryptocurrency(offers_data[i]['selling'])
+            sentence += ' '
+
+            sentence += ' for '
+            sentence += name_of_cryptocurrency(offers_data[i][key_buying])
+
+            sentence += ' at the price of '
+            sentence += offers_data[i]['price']
+            sentence += ' '
+
+            sentence += name_of_cryptocurrency(offers_data[i][key_selling])
+            sentence += '/'
+            sentence += name_of_cryptocurrency(offers_data[i][key_buying])
 
         print(sentence)
 
@@ -55,7 +99,9 @@ def selling_buying_offer(account_id):
 
 
 while True:
-    print('\n\n\n')
+    print('\n')
     selling_buying_offer(account_id)
+    print('\n')
+    values_of_pair()
     time.sleep(60)
 
--- quotes.py
+++ quotes.py
@@ -1,27 +1,29 @@
 import requests
 import json
 
-# Get market data from Stellar ticker
-r = requests.get('https://ticker.stellar.org/markets.json')
 
-json_obj = r.json()
+def values_of_pair(in_name_of_pair='XLM_XRP'):
+    # Get market data from Stellar ticker
+    r = requests.get('https://ticker.stellar.org/markets.json')
+    json_obj = r.json()
+    # For control
+    # print(json.dumps(json_obj, indent=4, sort_keys=True))
 
-# For control
-# print(json.dumps(json_obj, indent=4, sort_keys=True))
+    pairs = json_obj['pairs']
+    # print(json.dumps(pairs, indent=4, sort_keys=True))
+    # print(pairs)
 
-pairs = json_obj['pairs']
-# print(json.dumps(pairs, indent=4, sort_keys=True))
-# print(pairs)
+    for pair in pairs:
+        if pair['name'] == name_of_pair:
+            price_XLM_XRP = pair['price']
+            price_XRP_XLM = 1 / price_XLM_XRP
+
+            print('XLM / XRP = ' + "{:.18f}".format(price_XLM_XRP))
+            print('XRP / XLM = ' + "{:.18f}".format(price_XRP_XLM))
+
 
 name_of_pair = 'XLM_XRP'
-
-for pair in pairs:
-    if pair['name'] == name_of_pair:
-        price_XLM_XRP = pair['price']
-        price_XRP_XLM = 1/price_XLM_XRP
-
-        print('XLM / XRP = ' + "{:.18f}".format(price_XLM_XRP))
-        print('XRP / XLM = ' + "{:.18f}".format(price_XRP_XLM))
+values_of_pair(name_of_pair)
 
 
 
